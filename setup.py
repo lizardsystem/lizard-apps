1018bb36e5ece4d3f0f2508bfc73f7d02e4eba97
byrman <carsten.byrman@nelen-schuurmans.nl>
2016-02-17 11:30:03 +0100

    Update setup.py
    Set version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ install_requires = [
 
 setup(
     name='lizard-apps',
-    version='0.1',
+    version='0.1.2',
     packages=['lizard_apps'],
     include_package_data=True,
     license='MIT License',
