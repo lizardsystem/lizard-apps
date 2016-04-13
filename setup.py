ac378d69a074fb84c7c6b30fa656c81c92075716
byrman <carsten.byrman@nelen-schuurmans.nl>
2016-04-13 12:45:06 +0200

    Prepare release 0.2.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ install_requires = [
 
 setup(
     name='lizard-apps',
-    version='0.2.1',
+    version='0.2.2',
     packages=['lizard_apps'],
     include_package_data=True,
     license='MIT License',
