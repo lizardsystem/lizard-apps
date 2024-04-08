5dbb49434a5243233101b04ae6ce0d525480bbf0
byrman <carsten.byrman@nelen-schuurmans.nl>
2024-04-08 12:21:11 +0200

    Fix image preview

--- lizard_apps/admin.py
+++ lizard_apps/admin.py
@@ -1,47 +1,44 @@
 # -*- coding: utf-8 -*-
 # (c) Nelen & Schuurmans, see LICENSE.rst.
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
+from __future__ import absolute_import, division, print_function, unicode_literals
 
-from django.conf import settings
 from django.contrib import admin
-
+from django.utils.safestring import mark_safe
 from lizard_apps import models
 
 
 def get_image_tag(app):
     if app.icon:
-        return '<img src="{}{}" />'.format(settings.MEDIA_URL, app.icon)
+        return '<img src="{}" />'.format(app.icon.url)
     else:
-        return '-'
+        return "-"
 
 
 class ApplicationAdmin(admin.ModelAdmin):
-    list_display = ['name', 'url']
-    readonly_fields = ['image_tag']
+    list_display = ["name", "url"]
+    readonly_fields = ["image_tag"]
 
+    @mark_safe
     def image_tag(self, obj):
         return get_image_tag(obj)
 
     image_tag.allow_tags = True
-    image_tag.short_description = 'Preview'
+    image_tag.short_description = "Preview"
 
 
 class ApplicationScreenInline(admin.TabularInline):
     model = models.ApplicationScreen
-    readonly_fields = ['image_tag']
-    verbose_name = 'Application'
-    verbose_name_plural = 'Applications'
+    readonly_fields = ["image_tag"]
+    verbose_name = "Application"
+    verbose_name_plural = "Applications"
     extra = 1
 
     def image_tag(self, obj):
         return get_image_tag(obj.application)
 
     image_tag.allow_tags = True
-    image_tag.short_description = 'Preview'
+    image_tag.short_description = "Preview"
 
 
 class ScreenAdmin(admin.ModelAdmin):
